7a78f211e2d0d94ac08947ffef559428a68314c8
Duc Tran <t.minhduc2007@gmail.com>
2026-08-26 12:25:17 +0100

    v0.4.0 introduced multiple pedestrians in same grid and BFS pathfinding, no RL guidance yet

--- grid_baslines.py
+++ grid_baslines.py
@@ -10,15 +10,21 @@ truncated = False
 totalReward = 0
 episodeRewards= []
 episodeSteps = []
-for i in range(10000000):
+for i in range(100000):
+    obs,info = env.reset()
+    terminated= False
+    truncated = False
+    totalReward = 0
     while (terminated == False and truncated == False):
 
-        rand = np.random.randint(0,4)
-        action = rand
-
-        obs,reward,terminated,truncated,info = env.step(action)
+        obs,reward,terminated,truncated,info = env.step(0)
         totalReward += reward
+        print(
+            "step:", env.current_step,
+            "positions:", env.pedestrian_positions
+        )
     episodeRewards.append(totalReward)
     episodeSteps.append(env.current_step)
-    print(i)        
-print(f"Random Average reward: {np.mean(episodeRewards)}, Average steps: {np.mean(episodeSteps)}")
+    print(i)
+        
+print(f"BFS Average reward: {np.mean(episodeRewards)}, Average steps: {np.mean(episodeSteps)}, No of 8,9,10 steps episodes {episodeSteps.count(8)} {episodeSteps.count(9)} {episodeSteps.count(10)}")
--- grid_evac_env.py
+++ grid_evac_env.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
+from collections import deque
 
 class GridEvacEnv(gym.Env):
     def __init__(self):
@@ -14,55 +15,102 @@ class GridEvacEnv(gym.Env):
                 [1,0,0,0,0,2,1],
                 [1,1,1,1,1,1,1]
             ])
-        self.start_pos = (1,1)
+        self.start_positions = [
+            (1,1),
+            (1,2),
+            (1,3)
+        ]
+        
         self.action_space= spaces.Discrete(4)
         self.max_step = 1000
-        rows, col = self.grid.shape
-        self.observation_space= spaces.MultiDiscrete([rows,col])
+        rows, cols = self.grid.shape
+        self.observation_space= spaces.MultiDiscrete([rows,cols])
+        self.build_distance_map()
+
+    def build_distance_map(self):
+        rows, cols = self.grid.shape
+        self.distance_map = np.full((rows,cols),np.inf)
+        build_queue = deque([(rows-2,cols-2,0)])
+        self.distance_map[rows-2,cols-2] = 0
+        while len(build_queue) != 0:
+            row,col,dist = build_queue.pop()
+
+            # If left neighbor cell is not wall and is not visited
+            if (self.grid[row,col-1] != 1) and (self.distance_map[row,col-1] ==np.inf):
+                build_queue.appendleft((row,col-1,dist+1))
+                self.distance_map[row,col-1] = dist+1
+
+            # If right neighbor cell is not wall and is not visited 
+            if (self.grid[row,col+1] != 1) and (self.distance_map[row,col+1] ==np.inf):
+                build_queue.appendleft((row,col+1,dist+1))
+                self.distance_map[row,col+1] = dist+1
+
+            # If up neighbor cell is not wall and is not visited
+            if (self.grid[row-1,col] != 1) and (self.distance_map[row-1,col] ==np.inf):
+                build_queue.appendleft((row-1,col,dist+1))
+                self.distance_map[row-1,col] = dist+1
+
+            # If down neighbor cell is not wall and is not visited
+            if (self.grid[row+1,col] != 1) and (self.distance_map[row+1,col] ==np.inf):
+                build_queue.appendleft((row+1,col,dist+1))
+                self.distance_map[row+1,col] = dist+1
+
+
+    def choose_pedestrian_move(self,position):
+        row,col = position
+        chosen_position = position
+
+        #  Check left neighbor
+        if (self.grid[row,col-1] != 1) and (row,col-1) not in self.pedestrian_positions:
+            if self.distance_map[row,col-1] < self.distance_map[chosen_position]:
+                chosen_position = (row,col-1)
+        # check right neighbor
+        if (self.grid[row,col+1] != 1) and (row,col+1) not in self.pedestrian_positions:
+            if self.distance_map[row,col+1] < self.distance_map[chosen_position]:
+                chosen_position = (row,col+1)
+        # check up neighbor
+        if (self.grid[row-1,col] != 1) and (row-1,col) not in self.pedestrian_positions:
+            if self.distance_map[row-1,col] < self.distance_map[chosen_position]:
+                chosen_position = (row-1,col)
+        # check down neighbor
+        if (self.grid[row+1,col] != 1) and (row+1,col) not in self.pedestrian_positions:
+            if self.distance_map[row+1,col] < self.distance_map[chosen_position]:
+                chosen_position = (row+1,col)
+        return chosen_position
+        
+
+
+    
     def reset(self, seed=None,options=None):
         super().reset(seed=seed)
-        self.pedestrian_pos = self.start_pos
+        self.pedestrian_positions = self.start_positions.copy()
         self.current_step = 0
-        obs = np.array(self.pedestrian_pos)
+        obs = np.array(self.pedestrian_positions)
         info = {}
         return (obs,info)
     def step(self,action):
-        assert self.action_space.contains(action)
         terminated = False
         truncated =  False
-        row, col = self.pedestrian_pos
+        order = list(range(len(self.pedestrian_positions)))
+        self.np_random.shuffle(order)
+        for i in order:
+            if self.pedestrian_positions[i] is None:
+                continue
+            new_pos = self.choose_pedestrian_move(self.pedestrian_positions[i])
+            if self.grid[new_pos] == 2:
+                self.pedestrian_positions[i] = None
+            else:
+                self.pedestrian_positions[i] = new_pos
 
-        # Moving pedestrian
-        if action == 0:
-            new_pos = (row-1,col)
-        elif action ==1:
-            new_pos = (row,col+1)
-        elif action == 2:
-            new_pos = (row+1,col)
-        else:
-            new_pos = (row,col-1)
+        self.pedestrian_positions = [pos for pos in self.pedestrian_positions if pos is not None]
 
-        # Valid move check
-        rowBorder,colBorder = self.grid.shape
-        if new_pos[0] < 0 or new_pos[0]>=rowBorder or new_pos[1] <0 or new_pos[1]>=colBorder:
-            # invalid,out of border, but should not be checked for this grid cuz its wall surround border
-            new_pos = self.pedestrian_pos
-        elif self.grid[new_pos] == 1:
-            # hit wall
-            new_pos = self.pedestrian_pos
-        elif self.grid[new_pos] == 2:
-            self.pedestrian_pos = new_pos
-            terminated = True
 
-        else:
-            # valid move
-            self.pedestrian_pos = new_pos
         self.current_step +=1
+        terminated = len(self.pedestrian_positions) ==0
         truncated = self.current_step >= self.max_step
         reward = -1
-        obs= np.array(self.pedestrian_pos)
+        obs= self.pedestrian_positions
         info = {}
         return obs,reward,terminated,truncated,info
         
-
         
